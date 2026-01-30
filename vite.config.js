8472bfd1b9e65a412c897f9f7fdb9f8f4b2a2a84
shreshta-2008 <sheshtapapineni@gmail.com>
2026-01-30 09:19:09 +0530

    git commit -m "fix base path for GitHub Pages"

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ThisIsEnd`/'
+  base: '/ThisIsEnd/'   // 👈 THIS is the fix
 })
